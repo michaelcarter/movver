294d598b192c406c285a1c8563a2a8b49ced8173
MIke Carter <mike@mcarter.me>
2014-11-04 20:29:29 +0000

    Improved accuracy.

--- src/movver.js
+++ src/movver.js
@@ -1,24 +1,27 @@
 var Movver = (function() {
 
-  Movver.prototype.attachedElements = [];
+  Movver.prototype.watchedElements = [];
   Movver.prototype.touchStartOffsets = [];
   Movver.prototype.touchEndOffsets = [];
+  Movver.prototype.elementTimeouts = [];
   Movver.prototype.viewableLowerBoundary = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-  Movver.prototype.MAX_TOUCH_OFFSETS = 2;
-  Movver.prototype.FOCUS_PERCENTAGE_THRESHOLD = 80;
+  Movver.prototype.MAX_TOUCH_OFFSETS = 3;
+  Movver.prototype.FOCUS_PERCENTAGE_THRESHOLD = 70;
   Movver.prototype.DEBUG = false;
+  Movver.prototype.EVENT_TIMEOUT = 500;
 
   function Movver(opts) {
+    if (!opts) opts = {};
     if (opts.maxTouchOffsets) this.MAX_TOUCH_OFFSETS = opts.maxTouchOffsets;
     if (opts.focusPercentageThreshold) this.FOCUS_PERCENTAGE_THRESHOLD = opts.focusPercentageThreshold;
+    if (opts.eventTimeout) this.EVENT_TIMEOUT = opts.eventTimeout;
     if (!!opts.debug) this.DEBUG = true;
     this._addListeners();
   }
 
-  Movver.prototype.attach = function(element, callback) {
+  Movver.prototype.watch = function(element) {
     if (typeof element !== "object") throw new Error("Element must be an object.");
-    if (typeof callback !== "function") throw new Error("Callback must be a function.");
-    this.attachedElements.push([element, callback]);
+    this.watchedElements.push(element);
   }
 
   // PRIVATE FUNCTIONS
@@ -36,6 +39,34 @@ var Movver = (function() {
     return (result < 0) ? 0 : result;
   }
 
+  Movver.prototype._findElementTimeout = function(element) {
+    for (var i = 0; i < this.elementTimeouts.length; i++) {
+      if (element === this.elementTimeouts[i].element) {
+        return this.elementTimeouts[i];
+      }
+    }
+    return null;
+  } 
+
+  Movver.prototype._elementTimeoutExists = function(element) {
+    return !!this._findElementTimeout(element);
+  }
+
+  Movver.prototype._clearElementTimeout = function(element) {
+    var elementTimeout = this._findElementTimeout(element);
+    var indexOfElementTimeout = this.elementTimeouts.indexOf(elementTimeout);
+
+    if (elementTimeout) {
+      window.clearTimeout(elementTimeout.timeout);
+    }
+
+    if (indexOfElementTimeout > -1) {
+      this.elementTimeouts.splice(indexOfElementTimeout,1);
+    }
+
+    return !!elementTimeout;
+  }
+
   // DEBUG only functions, for highlighting what's focussed on the page.
   // =================================================================== //
   Movver.prototype._fillRect = function(element, focusPercentage) {
@@ -51,43 +82,50 @@ var Movver = (function() {
 
   // Document listeners
   // ================== //
-  Movver.prototype._addListeners = function() {
+  Movver.prototype._onTouchStart = function(e) {
+    this.touchStartOffsets.push(e.touches[0].pageY - e.view.scrollY);
+    if (this.touchStartOffsets.length > this.MAX_TOUCH_OFFSETS) this.touchStartOffsets.shift();
+  }
+
+  Movver.prototype._onTouchEnd = function(e) {
+    this.touchEndOffsets.push(e.changedTouches[0].pageY - e.view.scrollY);
+    if (this.touchEndOffsets.length > this.MAX_TOUCH_OFFSETS) this.touchEndOffsets.shift();
+    this.viewableLowerBoundary = Math.min(Math.round(this._arrayAverage(this.touchEndOffsets)), Math.round(this._arrayAverage(this.touchStartOffsets)));
+    if (this.DEBUG) this._setVisibleBounds();
+  }
+
+  Movver.prototype._onScroll = function() {
     var self = this;
 
-    document.addEventListener('touchstart', function(e) {
-      self.touchStartOffsets.push(e.touches[0].pageY - e.view.scrollY);
-      if (self.touchStartOffsets.length > self.MAX_TOUCH_OFFSETS) self.touchStartOffsets.shift();
-    });
+    // Trigger the event on any focussed elements.
+    this.watchedElements.forEach(function(element, index) {
+      var focusPercentage = self._calculateFocusPercentage(element);
+      var elementTimeoutExists = self._elementTimeoutExists(element);
+      if ((focusPercentage >= self.FOCUS_PERCENTAGE_THRESHOLD) && !elementTimeoutExists) {
 
-    document.addEventListener('touchend', function(e) {
-      self.touchEndOffsets.push(e.changedTouches[0].pageY - e.view.scrollY);
-      if (self.touchEndOffsets.length > self.MAX_TOUCH_OFFSETS) self.touchEndOffsets.shift();
-      self.viewableLowerBoundary = Math.min(Math.round(self._arrayAverage(self.touchEndOffsets)), Math.round(self._arrayAverage(self.touchStartOffsets)));
-      if (self.DEBUG) self._setVisibleBounds();
-    });
+        // Push a timeout to trigger the event onto the stack
+        self.elementTimeouts.push({
+          element: element,
+          timeout: window.setTimeout(function() {
+            element.dispatchEvent(new Event('movver'));
+          }, self.EVENT_TIMEOUT)
+        });
+      } else if ((focusPercentage < self.FOCUS_PERCENTAGE_THRESHOLD) && elementTimeoutExists) {
+        self._clearElementTimeout(element);
+      }
 
-    window.onscroll = function() {
-      self.attachedElements.forEach(function(elAndCallback) {
-        var el = elAndCallback[0];
-        var callback = elAndCallback[1];
-        var focusPercentage = self._calculateFocusPercentage(el);
-        if (focusPercentage >= self.FOCUS_PERCENTAGE_THRESHOLD) {
-          callback(el);
-        }
-        if (self.DEBUG) self._fillRect(el, focusPercentage);
-      });
-    }  
+      // Show the focus of the element visually.
+      if (self.DEBUG) {
+        self._fillRect(element, focusPercentage);
+      }
+    });
+  }
+
+  Movver.prototype._addListeners = function() {
+    document.addEventListener('touchstart', this._onTouchStart.bind(this));
+    document.addEventListener('touchend', this._onTouchEnd.bind(this));
+    window.onscroll = this._onScroll.bind(this);
   }
 
   return Movver;
 })();
-
-window.onload = function() {
-  var movver = new Movver({debug: true});
-  var elementIds = ["para1", "para2", "para3", "para4", "para5", "para6", "para7", "para8", "para9", "para10"];
-  elementIds.forEach(function(id) {
-    movver.attach(document.getElementById(id), function(el) {
-      console.log("focussed:", el);
-    });
-  });
-}
